ea516de7ff3ee96aef24435647c2b3ac6fc67b1f
Murat Ertik <dmuratertik1@gmail.com>
2026-01-17 22:45:49 +0300

    register method has been completed.

--- AuthMicroservice/src/main/java/com/example/controllers/AuthController.java
+++ AuthMicroservice/src/main/java/com/example/controllers/AuthController.java
@@ -1,7 +1,13 @@
 package com.example.controllers;
 
+import com.example.dto.requests.RegisterRequest;
+import com.example.dto.responses.RegisterResponse;
 import com.example.services.AuthService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class AuthController {
     private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<RegisterResponse> register(@RequestBody @Valid RegisterRequest request){
+        RegisterResponse response = authService.register(request);
+        return ResponseEntity.ok(response);
+
+
+    }
 
 }
