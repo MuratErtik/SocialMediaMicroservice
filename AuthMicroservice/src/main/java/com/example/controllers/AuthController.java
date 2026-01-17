bc106a2d029ab1dd8fb7c86b32b1c543dfb55998
Murat Ertik <dmuratertik1@gmail.com>
2026-01-17 20:57:20 +0300

    auth controller has been created.

---
+++ AuthMicroservice/src/main/java/com/example/controllers/AuthController.java
@@ -0,0 +1,14 @@
+package com.example.controllers;
+
+import com.example.services.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping
+public class AuthController {
+    private final AuthService authService;
+
+}
