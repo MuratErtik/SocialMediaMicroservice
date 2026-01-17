d334846d1942020e60a472e0646df9ee65b5a616
Murat Ertik <dmuratertik1@gmail.com>
2026-01-17 20:57:37 +0300

    auth service has been created.

---
+++ AuthMicroservice/src/main/java/com/example/services/AuthService.java
@@ -0,0 +1,14 @@
+package com.example.services;
+
+import com.example.repositories.AuthRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final AuthRepository authRepository;
+
+
+}
