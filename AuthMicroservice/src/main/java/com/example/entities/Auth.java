df7db18d79f76e68a989f075738740516baf6698
Murat Ertik <dmuratertik1@gmail.com>
2026-01-17 20:57:08 +0300

    auth entity has been created.

---
+++ AuthMicroservice/src/main/java/com/example/entities/Auth.java
@@ -0,0 +1,34 @@
+package com.example.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "tbl_auth")
+public class Auth {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String username;
+
+    String password;
+
+    String email;
+
+    Boolean isActive;
+
+    Long createdAt;
+
+
+
+
+}
